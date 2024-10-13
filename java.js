ae7743c07eb9eddce58a7d38358199354f3c034a
JyotiSoni <99126311+jyotijauharii@users.noreply.github.com>
2024-10-13 17:46:25 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,100 @@
+
+// function add_to_cart(pid,pname,price)
+// {
+//     localStorage.grtItem("cart");
+// }
+//     if(cart==null){
+// // no cart yet
+//     let products =[];
+//     let product ={productId:pid,     productName:pname,productQuantity:1,    productprice:price}
+
+//     products.push(product);
+//     localStorage.setItem("cart",JSON.stringify(products));
+//     }
+
+//     else{
+//     }
+//cart  
+
+var x =2;
+function myName() {
+    console.log("Helllo Mini")
+}
+
+myName();
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.onscroll = function() {myFunction()};
+
+// // get the navbar
+// var navbar = document.getElementById("navbar");
+
+// // get the offset position of navbar
+// var sticky = navbar.offsetTop;
+
+// // remove sticky when you leave the scroll bar
+// function myFunction(){
+//     if(window.pageYOffset >= sticky){
+//         navbar.classList.add("sticky")
+        
+//     }else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
+
+// alert("hello")
+
+// function myFunction(){
+//     var x =document.getElementById("navbar");
+//     if (x.className === "mid"){
+//         x.className += "responsive";
+//     }
+//     else{
+//         x.className ="mid";
+//     }
+// }
+
+// function myFunction1(){
+//     var y =document.getElementById("column");
+//     if(y.className ==="card"){
+//         y.className += "responsive";
+//     }
+//     else{
+//         y.className ="card"
+//     }
+// }
